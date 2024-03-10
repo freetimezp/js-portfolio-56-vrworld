adbffa4fcd1e2fa32f17de5f19a4708d4d361fa8
freetimezp <kraftukrnet@ukr.net>
2024-03-10 20:03:13 +0200

    04. js-portfolio-56-vrworld. Hero component. AOS animation.

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,32 @@
-import Navbar from "./components/Navbar/Navbar";
+import { useEffect, useState } from "react";
+import AOS from "aos";
+import "aos/dist/aos.css";
 
+import Navbar from "./components/Navbar/Navbar";
+import Hero from "./components/Hero/Hero";
 
 function App() {
+  const [isPlay, setIsPlay] = useState(false);
+
+  const togglePlay = () => {
+    setIsPlay(!isPlay);
+  };
+
+  useEffect(() => {
+    AOS.init({
+      offset: 100,
+      duration: 800,
+      easing: "ease-in-sine",
+      delay: 100,
+    });
+    AOS.refresh();
+  }, []);
 
   return (
-    <main className="overflow-x-hidden">
+    <main className="overflow-x-hidden bg-white dark:bg-black text-black 
+      dark:text-white duration-300">
       <Navbar />
-
+      <Hero togglePlay={togglePlay} />
     </main>
   );
 }
